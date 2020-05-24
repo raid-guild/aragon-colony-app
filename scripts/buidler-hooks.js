e912a796234907844db5ba21cd72cf487feef87e
dapplion <guillem.lion.benosa@gmail.com>
2020-05-25 01:20:33 +0200

    Use Empty Colony contract instead of Counter

--- scripts/buidler-hooks.js
+++ scripts/buidler-hooks.js
@@ -36,9 +36,13 @@ module.exports = {
   // Called when the start task needs to know the app proxy's init parameters.
   // Must return an array with the proxy's init parameters.
   getInitParams: async ({ log }, { web3, artifacts }) => {
-    return [42]
+    return [
+      "0x0000000000000000000000000000000000000000",
+      "0x0000000000000000000000000000000000000000",
+      "0x0000000000000000000000000000000000000000",
+    ];
   },
 
   // Called after the app's proxy is updated with a new implementation.
   postUpdate: async ({ proxy, log }, { web3, artifacts }) => {},
-}
+};
